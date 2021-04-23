218a8451433e8d7fb9f3497a41cb2a225a1729a7
wtprogrammer7 <73503565+wtprogrammer7@users.noreply.github.com>
2021-04-23 10:21:12 -0700

    Update hooks.py

--- ssg/hooks.py
+++ ssg/hooks.py
@@ -2,8 +2,6 @@ _callbacks = {}
 
 def register(hook, order = 0):
     def register_callback(func):
-        _callbacks.setdefault(hook, {})
-        _callbacks.setdefault(order, [])
-        _callbacks.append(func)
+        _callbacks.setdefault(hook, {}).setdefault(order, []).append(func)
         return func
     return register_callback
