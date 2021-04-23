6949d98108bd2363e2509408dc68dd8c7617dd70
wtprogrammer7 <73503565+wtprogrammer7@users.noreply.github.com>
2021-04-23 10:56:04 -0700

    Update menu.py

--- ssg/extensions/menu.py
+++ ssg/extensions/menu.py
@@ -14,3 +14,4 @@ def collect_files(source, site_parsers):
 def generate_menu(html, ext):
     template = '<li><a href="{}{}">{}</a></li>'
     menu_item = lambda name, ext: template.format(name, ext, name.title())
+    menu = "\n".join(menu_item(path.stem, ext), [for path in files])
