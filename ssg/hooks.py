1167959f92871ae8809731da58feaf5b106bd02a
wtprogrammer7 <73503565+wtprogrammer7@users.noreply.github.com>
2021-04-23 10:26:56 -0700

    Update hooks.py

--- ssg/hooks.py
+++ ssg/hooks.py
@@ -10,3 +10,10 @@ def event(hook, *args):
     for order in sorted(_callbacks.get(hook, {})):
         for func in _callbacks[hook][order]:
             func(*args)
+
+def filter(hook, value, *args):
+    for order in sorted(_callbacks.get(hook, {})):
+        for func in _callbacks[hook][order]:
+            value = func(value, *args)
+
+    return value
