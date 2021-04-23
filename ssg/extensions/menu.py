1f46fdbf2004f81b411da17922d95139a6350d9d
wtprogrammer7 <73503565+wtprogrammer7@users.noreply.github.com>
2021-04-23 10:41:48 -0700

    Update menu.py

--- ssg/extensions/menu.py
+++ ssg/extensions/menu.py
@@ -4,4 +4,4 @@ files = []
 
 @hooks.register("collect_files")
 def collect_files(source, site_parsers):
-    valid = lambda p: not isinstance(parsers.ResourceParser)
+    valid = lambda p: not isinstance(p, parsers.ResourceParser)
