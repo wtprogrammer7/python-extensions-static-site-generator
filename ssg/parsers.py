ef958040b50d3ac9bde2910c7cfcfe3e703e2567
wtprogrammer7 <73503565+wtprogrammer7@users.noreply.github.com>
2021-04-23 11:04:57 -0700

    Update parsers.py

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -8,6 +8,7 @@ from docutils.core import publish_parts
 from markdown import markdown
 from ssg.content import Content
 
+from ssg import hooks
 
 class Parser:
     base_ext = ".html"
@@ -45,7 +46,8 @@ class MarkdownParser(Parser):
     def parse(self, path, source, dest):
         content = Content.load(self.read(path))
         html = markdown(content.body)
-        self.write(path, dest, html)
+        filtered = hooks.filter("generate_menu", html, extension(self.base_ext))
+        self.write(path, dest, filtered)
         sys.stdout.write(
             "\x1b[1;32m{} converted to HTML. Metadata: {}\n".format(path.name, content)
         )
