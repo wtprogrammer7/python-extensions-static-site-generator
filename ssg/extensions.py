edd9e3cb57394d937beeb6469de32bb1dd1d35d2
wtprogrammer7 <73503565+wtprogrammer7@users.noreply.github.com>
2021-04-23 10:05:47 -0700

    Update extensions.py

--- ssg/extensions.py
+++ ssg/extensions.py
@@ -6,3 +6,7 @@ def load_module(directory, name):
     sys.path.insert(0, directory)
     importlib.import_module(name)
     sys.path.pop(0)
+
+def load_directory(directory):
+    for path in directory.rglob("*.py"):
+        load_module(directory.as_posix(), path.stem)
