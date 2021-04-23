3dda8f7385d2320171f1c014cd895548e0c6243f
wtprogrammer7 <73503565+wtprogrammer7@users.noreply.github.com>
2021-04-23 10:49:00 -0700

    Update menu.py

--- ssg/extensions/menu.py
+++ ssg/extensions/menu.py
@@ -9,3 +9,7 @@ def collect_files(source, site_parsers):
         for parser in list(filter(valid, site_parsers)):
             if parser.valid_file_ext(path.suffix):
                 files.append(path)
+
+@hook.register("generate_menu")
+def generate_menu(html, ext):
+    template = '<li><a href="{}{}">{}</a></li>'
