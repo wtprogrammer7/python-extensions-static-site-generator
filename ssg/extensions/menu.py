9397ee35e6fc760d4fc96df3be67f9a0a4375cdc
wtprogrammer7 <73503565+wtprogrammer7@users.noreply.github.com>
2021-04-23 10:52:01 -0700

    Update menu.py

--- ssg/extensions/menu.py
+++ ssg/extensions/menu.py
@@ -13,3 +13,4 @@ def collect_files(source, site_parsers):
 @hooks.register("generate_menu")
 def generate_menu(html, ext):
     template = '<li><a href="{}{}">{}</a></li>'
+    menu_item = lambda name, ext: template.format(name, ext, name.title())
