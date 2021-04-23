131dea0f72bdc9c71cc01fd96095e1e80d6ab907
wtprogrammer7 <73503565+wtprogrammer7@users.noreply.github.com>
2021-04-23 10:09:23 -0700

    Update extensions.py

--- ssg/extensions.py
+++ ssg/extensions.py
@@ -10,3 +10,6 @@ def load_module(directory, name):
 def load_directory(directory):
     for path in directory.rglob("*.py"):
         load_module(directory.as_posix(), path.stem)
+
+def load_bundled():
+    directory = Path(__file__).parent
