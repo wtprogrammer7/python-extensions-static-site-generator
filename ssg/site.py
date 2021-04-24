1468c3a9881ec4c868a1e3137452442ac3af7582
wtprogrammer7 <73503565+wtprogrammer7@users.noreply.github.com>
2021-04-24 10:47:45 -0700

    Update site.py

--- ssg/site.py
+++ ssg/site.py
@@ -29,12 +29,14 @@ class Site:
     def build(self):
         extensions.load_bundled()
         hooks.event("collect_files", self.source, self.parsers)
+        "start_build"
         self.dest.mkdir(parents=True, exist_ok=True)
         for path in self.source.rglob("*"):
             if path.is_dir():
                 self.create_dir(path)
             elif path.is_file():
                 self.run_parser(path)
+        "stats"
 
     @staticmethod
     def error(message):
